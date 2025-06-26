e275be031712286905db0dd839c559a79e2f14af
Hassan Qureshi <hassanqureshi21@gmail.com>
2025-06-26 08:10:29 +0530

    Initial commit

---
+++ src/main/java/com/example/chronoscouriers/chronoscouriers/ChronoscouriersApplication.java
@@ -0,0 +1,13 @@
+package com.example.chronoscouriers.chronoscouriers;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChronoscouriersApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChronoscouriersApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/chronoscouriers/chronoscouriers/ChronoscouriersApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.chronoscouriers.chronoscouriers;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChronoscouriersApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
